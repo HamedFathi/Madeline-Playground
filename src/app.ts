09192b57ce545c9b9498cc3b685ffd5a6034b9ad
hamedfathi <noeffect@outlook.com>
2019-08-23 20:12:52 +0430

    update: convert functionality

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,17 @@ import { SourceFileExtractor } from 'madeline';
 export class App {
   public message: string = 'Hello World!';
 
-  public convert(text: string): string {
+  private resultElem: HTMLTextAreaElement;
+
+  private convert() {
+    //@ts-ignore
+    let text = document.getElementById('editor').value;
+    this.resultElem.textContent = "";
     let srcInfo = new SourceFileExtractor().extractFromText(text);
-    let result = JSON.stringify(srcInfo);
-    return result;
+    let result = JSON.stringify(srcInfo, null, 2);
+    this.resultElem.textContent = result;
   }
+
+
+
 }
