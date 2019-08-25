d5f42aa3a19b1cf0f7e459ed878647b505bf2f43
Hamed Fathi <h.fathi@tax.gov.ir>
2019-08-25 10:36:30 +0430

    update: playground sample.

--- src/app.ts
+++ src/app.ts
@@ -5,22 +5,21 @@ import { inject, TaskQueue } from 'aurelia-framework';
 export class App {
 
   private myTsJsSource: string = `/**
-  * This is a class.
-  * Just for test!
-  *
-  * @class Greeter - A class with ctor and a method.
-  */
-  class Greeter {
-    greeting: string;
-    constructor(message: string) {
-      this.greeting = message;
-    }
-    // Just says hello :)
-    greet() {
-      return "Hello, " + this.greeting;
-    }
-  }`
-    ;
+* This is a class.
+* Just for test!
+*
+* @class Greeter - A class with ctor and a method.
+*/
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  // Just says hello :)
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}`;
 
   constructor(private taskQueue: TaskQueue) { }
 
